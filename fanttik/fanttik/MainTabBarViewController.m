b6727269911fdb54667b5302b9816b15ac7164c5
luoluya <luoluya@163.com>
2023-05-13 11:04:00 +0800

    "首页提交"

---
+++ fanttik/fanttik/MainTabBarViewController.m
@@ -0,0 +1,46 @@
+//
+//  MainTableViewController.m
+//  fanttik
+//
+//  Created by 罗路雅 on 2023/5/12.
+//
+
+#import "MainTabBarViewController.h"
+
+@interface MainTabBarViewController ()
+
+@end
+
+@implementation MainTabBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
